f235a1a84acd50c7f1080355d3158ea0ec2bcec2
kyle <Kyle>
2023-02-25 17:59:00 -0300

    moved bot startup process out of main as own service

--- src/Program.cs
+++ src/Program.cs
@@ -26,6 +26,8 @@ namespace FrenBot
                 .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig() { GatewayIntents = GatewayIntents.All }))
                 .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
                 .AddSingleton<InteractionHandler>()
+                .AddSingleton<StartupService>()
+                .AddSingleton<LoggingService>()
                 .AddSingleton<GuildConfigManager>()
                 .AddSingleton<VoiceChatNotifier>())
                 .Build();
@@ -44,40 +46,7 @@ namespace FrenBot
             serviceProvider.GetRequiredService<GuildConfigManager>();
             serviceProvider.GetRequiredService<VoiceChatNotifier>();
 
-            var manager = serviceProvider.GetRequiredService<GuildConfigManager>();
-            await manager.InitializeAsync();
-
-            var interactions = serviceProvider.GetRequiredService<InteractionService>();
-            await serviceProvider.GetRequiredService<InteractionHandler>().InitializeAsync();
-
-            var notifier = serviceProvider.GetRequiredService<VoiceChatNotifier>();
-            await notifier.InitializeAsync();
-
-            var config = serviceProvider.GetRequiredService<IConfigurationRoot>();
-
-            client.Log += Log;
-            interactions.Log += Log;
-
-            client.Ready += async () =>
-            {
-                Console.WriteLine("Hello, World!");
-
-                // gather guildids
-                var guilds = client.Guilds;
-
-                // register commands to servers
-                foreach (var guild in guilds)
-                {
-                    ulong guildID = guild.Id;
-
-                    await interactions.RegisterCommandsToGuildAsync(guildID);
-                    Console.WriteLine($"registered commands to guildID: {guildID}");
-                }
-            };
-            await client.LoginAsync(TokenType.Bot, config.GetSection("appConfig")["token"]);
-
-            await client.StartAsync();
-
+            await serviceProvider.GetRequiredService<StartupService>().StartUpAsync();
             await Task.Delay(-1);
         }
     }
---
+++ src/Services/StartupService.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using FrenBot.Modules;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+
+namespace FrenBot.Services
+{
+    public class StartupService
+    {
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interactions;
+        private readonly IConfigurationRoot _config;
+
+        public StartupService(IServiceProvider provider, DiscordSocketClient client, InteractionService interactions, IConfigurationRoot config)
+        {
+            _provider = provider;
+            _client = client;
+            _interactions = interactions;
+            _config = config;
+
+            _client.Ready += OnReadyAsync;
+        }
+
+        public async Task StartUpAsync()
+        {
+            var token = _config.GetSection("appConfig")["token"];
+            if (string.IsNullOrWhiteSpace(token))
+                throw new Exception("Please enter the bot's token into the 'config.json' file in the app's root directory.");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+
+        private async Task OnReadyAsync()
+        {
+            Console.WriteLine("Hello, World!");
+
+            // gather guildids
+            var guilds = _client.Guilds;
+
+            // register commands to servers
+            foreach (var guild in guilds)
+            {
+                ulong guildID = guild.Id;
+
+                await _interactions.RegisterCommandsToGuildAsync(guildID);
+                Console.WriteLine($"registered commands to guildID: {guildID}");
+            }
+        }
+    }
+}
