c5def1fa106ff08e6fde1968f03ee8f10a23368d
kyle <Kyle>
2023-02-25 16:16:24 -0300

    created logging service

---
+++ Services/LoggingService.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace FrenBot.Services
+{
+    public class LoggingService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interactions;
+
+        private string _logDirectory { get; }
+        private string _logFile => Path.Combine(_logDirectory, $"{DateTime.UtcNow:yyyy-MM-dd}.txt");
+
+        public LoggingService(DiscordSocketClient client, InteractionService interactions)
+        {
+            _logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
+
+            _client = client;
+            _interactions = interactions;
+
+            _client.Log += OnLogAsync;
+            _interactions.Log += OnLogAsync;
+        }
+
+        private Task OnLogAsync(LogMessage msg)
+        {
+            if (!Directory.Exists(_logDirectory))
+                Directory.CreateDirectory(_logDirectory);
+            if (!File.Exists(_logFile))
+                File.Create(_logFile).Dispose();
+
+            string logText = $"{DateTime.UtcNow:hh:mm:ss} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}";
+            File.AppendAllText(_logFile, logText + "\n");
+
+            return Console.Out.WriteLineAsync(logText);
+        }
+    }
+}
